17841d545d2b85140c4161a3eb94d339519ecd59
AdamSultan2020 <onyx-bi@hotmail.com>
2022-09-29 20:52:01 +1000

    Fixing Validator

--- infrastructure/ValidatorAttribute.cs
+++ infrastructure/ValidatorAttribute.cs
@@ -13,19 +13,19 @@ namespace ButterFlyApi.infrastructure
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            double input1 = 0.0;
-            double input2 = 0.0;
+            double input1 = 0;
+            double input2 = 0;
             int round = 0;
             if (!double.TryParse(context.ActionArguments["input1"].ToString().TrimEnd().TrimStart(), out input1))
                 context.Result = new UnprocessableEntityObjectResult("First input is not a number");
 
             if (!double.TryParse(context.ActionArguments["input2"].ToString().TrimEnd().TrimStart(), out input2))
                 context.Result = new UnprocessableEntityObjectResult("Second input is not a number");
-
+            
             if (context.RouteData.Values["action"].ToString().ToLower().Contains("withround") && !int.TryParse(context.ActionArguments["round"].ToString().TrimEnd().TrimStart(),out round))
                 context.Result = new BadRequestObjectResult("Round parameter is not a number");
 
-            if (context.RouteData.Values["action"].ToString().ToLower().Contains("dividing") && input2 == 0)
+            if (context.RouteData.Values["action"].ToString().ToLower().Contains("division") && input2 == 0)
                 context.Result = new BadRequestObjectResult("You cannot divide number by 0");
 
             if (context.RouteData.Values["action"].ToString().ToLower().Contains("withround") && round < 0)
